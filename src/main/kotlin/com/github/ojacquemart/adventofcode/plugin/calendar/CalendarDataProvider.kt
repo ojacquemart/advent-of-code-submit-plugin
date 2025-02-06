3f9a2b0bb76dcfc62a9e5ae08f74ac324c882bfb
Olivier Jacquemart <o.jacquemart@gmail.com>
2025-02-06 21:25:28 +0100

    fix: avoid event year selection if not in december

--- src/main/kotlin/com/github/ojacquemart/adventofcode/plugin/calendar/CalendarDataProvider.kt
+++ src/main/kotlin/com/github/ojacquemart/adventofcode/plugin/calendar/CalendarDataProvider.kt
@@ -1,7 +1,8 @@
 package com.github.ojacquemart.adventofcode.plugin.calendar
 
 import com.github.ojacquemart.adventofcode.plugin.Aoc
-import java.time.LocalDateTime
+import java.time.LocalDate
+import java.time.Month
 
 object CalendarDataProvider {
 
@@ -9,17 +10,21 @@ object CalendarDataProvider {
     data class DayHolder(val day: Int, val inFuture: Boolean)
 
     fun getYears(): List<YearHolder> {
-        val now = LocalDateTime.now(Aoc.NY_TIMEZONE_ID)
-        val currentYear = now.year
+        val now = LocalDate.now(Aoc.NY_TIMEZONE_ID)
+        val isDecember = now.month == Month.DECEMBER
+        val adventYear = if (isDecember) now.year else now.year - 1
         val currentDay = now.dayOfMonth
 
-        val isDayInFuture = { year: Int, day: Int -> year == currentYear && day > currentDay }
-
-        return (currentYear downTo Aoc.FIRST_YEAR).map { year ->
-            val days = Aoc.DAYS.map { day -> DayHolder(day, isDayInFuture(year, day)) }
-
-            YearHolder(year, days)
+        val isDayInFuture = { year: Int, day: Int ->
+            year == adventYear && isDecember && day > currentDay
         }
+
+        return (adventYear downTo Aoc.FIRST_YEAR)
+            .map { year ->
+                val days = Aoc.DAYS.map { day -> DayHolder(day, isDayInFuture(year, day)) }
+
+                YearHolder(year, days)
+            }
     }
 
 }
